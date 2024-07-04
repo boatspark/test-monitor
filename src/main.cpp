6cb183a82a26008a42077cb2f6e5668fd55ac5f6
Ade Bateman <bateman@acm.org>
2024-07-03 22:14:23 -0700

    Add publish code.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 
 BeaconScanner scanner;
 GPS gps;
+const char* prepareJSON();
 
 void setup() {
     delay(2000);
@@ -16,19 +17,39 @@ void setup() {
 
     scanner.start();
     gps.start();
+
+    Cellular.on();
+    Particle.connect();
 }
 
-char buf[512];
+const unsigned long PUBLISH_PERIOD = 300000;
+unsigned long lastPublish = 0;
+const unsigned long SERIAL_PERIOD = 5000;
+unsigned long lastSerial = 0;
 
 void loop() {
-    delay(2000);
+    if (millis() - lastSerial >= SERIAL_PERIOD) {
+        lastSerial = millis();
+        const char* json = prepareJSON();
+        Serial.println(json);
 
-    JSONBufferWriter json(buf, sizeof(buf) - 1);
+        if (Particle.connected()) {
+            if (millis() - lastPublish >= PUBLISH_PERIOD) {
+                lastPublish = millis();
+                Particle.publish("monitor", json);
+            }
+        }
+    }
+}
+
+char jsonbuf[512];
+const char* prepareJSON() {
+    JSONBufferWriter json(jsonbuf, sizeof(jsonbuf) - 1);
     json.beginObject();
     scanner.toJSON(&json);
     gps.toJSON(&json);
     json.endObject();
     size_t size = std::min(json.bufferSize(), json.dataSize());
     json.buffer()[size] = '\0';
-    Serial.println(buf);
+    return json.buffer();
 }
